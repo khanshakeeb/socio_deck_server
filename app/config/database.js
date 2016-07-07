416c66c486fb29a054b4efb4bdc314b4c57d302e
Muhammad Shakeeb Khan <khanshakeeb@msn.com>
2016-07-07 11:06:41 +0500

    add local login strategy, install twitter node module and also twitter configuration

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://social_deck:social_deck@ds011715.mlab.com:11715/social_deck' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    //'url' : 'mongodb://social_deck:social_deck@ds011715.mlab.com:11715/social_deck' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://social_deck:social_deck@localhost:27017/social_deck'
 
 };
